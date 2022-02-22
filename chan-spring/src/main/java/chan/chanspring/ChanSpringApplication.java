b656ff3ee7bdd34a855cfe19b5e2548a9bc4129f
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-02-22 16:03:53 +0900

    ResponseBody
    api study

---
+++ chan-spring/src/main/java/chan/chanspring/ChanSpringApplication.java
@@ -0,0 +1,14 @@
+package chan.chanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChanSpringApplication.class, args);
+	}
+
+}
---
+++ chan-spring/src/test/java/chan/chanspring/ChanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package chan.chanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
