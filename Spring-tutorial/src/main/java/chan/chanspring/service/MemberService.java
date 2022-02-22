92763151edd8d27d5e1fc3d40f63d273ed3fe0e5
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-02-22 19:47:14 +0900

    회원 서비스
    중복 회원 검증 및 등록

--- Spring-tutorial/src/main/java/chan/chanspring/service/MemberService.java
+++ Spring-tutorial/src/main/java/chan/chanspring/service/MemberService.java
@@ -7,13 +7,16 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     /**
      회원가입
      **/
     public Long join(Member member) {
-        validateDuplicateMember(member);  //중복 회원 검증 memberRepository.save(member);
+        validateDuplicateMember(member);  //중복 회원 검증
+        memberRepository.save(member);
         return member.getId();
     }
     private void validateDuplicateMember(Member member) {
