08619b3f224adacd455b782eb5ed3c4efe907ff0
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-02-23 17:39:48 +0900

    Annotation 주입
    컴포넌트 스캔

--- Spring-tutorial/src/main/java/chan/chanspring/service/MemberService.java
+++ Spring-tutorial/src/main/java/chan/chanspring/service/MemberService.java
@@ -2,12 +2,18 @@ package chan.chanspring.service;
 import chan.chanspring.domain.Member;
 import chan.chanspring.repository.MemberRepository;
 import chan.chanspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
+
     private final MemberRepository memberRepository;
+
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
