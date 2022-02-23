d24570e12c07a3dad791a70d077ad1c27a393c7e
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-02-23 18:16:01 +0900

    회원 등록 및 조회 컨트롤러
    mvc

---
+++ Spring-tutorial/src/main/java/chan/chanspring/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package chan.chanspring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
