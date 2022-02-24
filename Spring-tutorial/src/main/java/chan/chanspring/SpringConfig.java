453bd3090512253ce1cd0207bddb19588d20e6a3
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-02-24 18:24:27 +0900

    jap 사용

--- Spring-tutorial/src/main/java/chan/chanspring/SpringConfig.java
+++ Spring-tutorial/src/main/java/chan/chanspring/SpringConfig.java
@@ -1,20 +1,34 @@
 package chan.chanspring;
 
+import chan.chanspring.repository.JpaMemberRepository;
 import chan.chanspring.repository.MemberRepository;
 import chan.chanspring.repository.MemoryMemberRepository;
 import chan.chanspring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
-
 }
