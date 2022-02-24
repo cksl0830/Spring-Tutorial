4e983593a4c0850d154f84352e19f1d28e2f9e53
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-02-24 18:52:24 +0900

    SpringDataJPA 사용

---
+++ Spring-tutorial/src/main/java/chan/chanspring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package chan.chanspring.repository;
+
+import chan.chanspring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member,
+        Long>, MemberRepository {
+    Optional<Member> findByName(String name);
+}
